815f94291b801462b5b38384c0ebb062fcc2dac0
castillorosy <rosycastillo18@yahoo.com>
2015-04-22 14:01:38 -0700

    blaj

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -183,9 +183,19 @@ game.SpendGold = Object.extend({
         }
     },
     checkCost: function(skill) {
-        if (skill === 1) {
+        if (skill === 1 &&(game.data.gold >=((game.data.skill1+1)*10))) {
             return true;
-        } else {
+        }else if (skill === 2 &&(game.data.gold >=((game.data.skill2+1)*10))) {
+            return true;
+        }else if (skill === 3 &&(game.data.gold >=((game.data.skill3+1)*10))) {
+            return true;
+        }else if (skill === 4 &&(game.data.gold >=((game.data.skill4+1)*10))) {
+            return true;
+        }else if (skill === 5 &&(game.data.gold >=((game.data.skill5+1)*10))) {
+            return true;
+        }else if (skill === 6 &&(game.data.gold >=((game.data.skill6+1)*10))) {
+            return true;
+        }else {
             return false;
         }
     },
